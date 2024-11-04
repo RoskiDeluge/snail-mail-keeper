cbe46ef6c818dd6067472c37d3261f6e20645c34
RoskiDeluge <roberto.delgado@gmail.com>
2024-11-04 10:12:44 -1000

    Reconnected dev dv in mongo atlas

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
   return (
     <div>
-      <h1>About This App</h1>
-      <p className='my-1'>
-        This is a full stack React app for keeping contacts
+      <h1>About Snail Mail Keeper</h1>
+      <p className="my-1">
+        Snail Mail Keeper is a simple application that keeps track of your snail
+        mail.
       </p>
-      <p className='bg-dark p'>
-        <strong>Version: </strong> 1.0.0
+      <p className="bg-dark p">
+        <strong>Version: </strong> 0.0.1
       </p>
     </div>
   );
